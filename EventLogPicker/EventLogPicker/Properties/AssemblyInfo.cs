63491d3e7b3b1ef7caf7c0364c40b24938e979d2
Keiho Sakapon <git@saka-pon.net>
2017-04-21 15:38:03 +0900

    Update AssemblyInfo

--- EventLogPicker/EventLogPicker/Properties/AssemblyInfo.cs
+++ EventLogPicker/EventLogPicker/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("EventLogPicker")]
+[assembly: AssemblyTitle("Event Log Picker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EventLogPicker")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Keiho Sakapon")]
+[assembly: AssemblyProduct("KTools")]
+[assembly: AssemblyCopyright("© 2017 Keiho Sakapon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
