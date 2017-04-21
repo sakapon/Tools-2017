8bef48f314055017db2f79378a19e14ca311e5e6
Keiho Sakapon <git@saka-pon.net>
2017-04-21 14:34:46 +0900

    Create project

---
+++ EventLogPicker/EventLogPicker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventLogPicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
