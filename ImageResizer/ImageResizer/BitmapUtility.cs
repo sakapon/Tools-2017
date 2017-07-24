456c8ece054b16160699e8ee4e2da48680a3c575
Keiho Sakapon <git@saka-pon.net>
2017-07-24 23:06:11 +0900

    Add class

---
+++ ImageResizer/ImageResizer/BitmapUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace ImageResizer
+{
+    public static class BitmapUtility
+    {
+        public static Bitmap ResizeImage(Image source, Size size, InterpolationMode interpolationMode = InterpolationMode.Bilinear) =>
+            ResizeImage(source, size.Width, size.Height, interpolationMode);
+
+        public static Bitmap ResizeImage(Image source, int width, int height, InterpolationMode interpolationMode = InterpolationMode.Bilinear)
+        {
+            var bitmap = new Bitmap(width, height);
+
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.InterpolationMode = interpolationMode;
+                graphics.DrawImage(source, 0, 0, width, height);
+            }
+            return bitmap;
+        }
+    }
+}
