abc7c87bfd30fbfc73cde1633f9a1cebf5bdc9dd
Keiho Sakapon <git@saka-pon.net>
2017-07-24 23:54:46 +0900

    Add class

---
+++ ImageResizer/ImageResizer/BitmapHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+
+namespace ImageResizer
+{
+    public static class BitmapHelper
+    {
+        public static Bitmap ScaleImage(Image source, double scale) =>
+            BitmapUtility.ResizeImage(source, Round(scale * source.Width), Round(scale * source.Height));
+
+        public static int Round(this double value) =>
+            (int)Math.Round(value, MidpointRounding.AwayFromZero);
+    }
+}
