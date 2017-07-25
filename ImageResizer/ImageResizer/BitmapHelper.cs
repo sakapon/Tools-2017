4029792a5d1e2c1ca48918f16aea0c6a9ba5b7a7
Keiho Sakapon <git@saka-pon.net>
2017-07-25 18:52:50 +0900

    Add methods

--- ImageResizer/ImageResizer/BitmapHelper.cs
+++ ImageResizer/ImageResizer/BitmapHelper.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace ImageResizer
 {
     public static class BitmapHelper
     {
+        public static void ScaleImageFile(string sourceFile, string destFile, double scale)
+        {
+            using (var source = Image.FromFile(sourceFile))
+            using (var resized = ScaleImage(source, scale))
+            {
+                resized.Save(destFile, ImageFormat.Jpeg);
+            }
+        }
+
         public static Bitmap ScaleImage(Image source, double scale) =>
             BitmapUtility.ResizeImage(source, Round(scale * source.Width), Round(scale * source.Height));
 
