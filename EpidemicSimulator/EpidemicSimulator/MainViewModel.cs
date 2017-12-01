6e41e501555edbe27134c5ded151a43d47d9e2b1
Keiho Sakapon <git@saka-pon.net>
2017-12-01 23:04:34 +0900

    Implement MainViewModel

--- EpidemicSimulator/EpidemicSimulator/MainViewModel.cs
+++ EpidemicSimulator/EpidemicSimulator/MainViewModel.cs
@@ -1,13 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive.Linq;
+using Reactive.Bindings;
 
 namespace EpidemicSimulator
 {
     public class MainViewModel
     {
+        static readonly TimeSpan RenderingInterval = TimeSpan.FromSeconds(1 / 30.0);
+        const int PopulationBarWidth = 1000;
+
         public AppModel AppModel { get; } = new AppModel();
+
+        public ReactiveProperty<InfectionModel> InfectionModel { get; } = new ReactiveProperty<InfectionModel>(mode: ReactivePropertyMode.DistinctUntilChanged);
+        public ReadOnlyReactiveProperty<byte[]> PopulationImage { get; }
+        public ReadOnlyReactiveProperty<PopulationSummary> PopulationSummary { get; }
+        public ReadOnlyReactiveProperty<PopulationLayout> PopulationLayout { get; }
+
+        IDisposable subscription;
+
+        public MainViewModel()
+        {
+            PopulationImage = InfectionModel.Select(DataModel.GetBitmapBinary).ToReadOnlyReactiveProperty();
+            PopulationSummary = InfectionModel.Select(DataModel.ToSummary).ToReadOnlyReactiveProperty();
+            PopulationLayout = PopulationSummary.Select(ToLayout).ToReadOnlyReactiveProperty();
+
+            AppModel.IsRunning
+                .Where(b => b)
+                .Subscribe(b =>
+                {
+                    subscription = Observable.Interval(RenderingInterval)
+                        .Subscribe(_ => InfectionModel.Value = AppModel.CurrentInfection);
+                });
+            AppModel.IsRunning
+                .Where(b => !b)
+                .Subscribe(b =>
+                {
+                    subscription?.Dispose();
+                    subscription = null;
+                });
+        }
+
+        static PopulationLayout ToLayout(PopulationSummary s)
+        {
+            var width_s = (int)Math.Round(PopulationBarWidth * ((double)s.Susceptible / s.Total), MidpointRounding.AwayFromZero);
+            var width_i = (int)Math.Round(PopulationBarWidth * ((double)s.Infectious / s.Total), MidpointRounding.AwayFromZero);
+
+            return new PopulationLayout
+            {
+                SusceptibleWidth = width_s,
+                InfectiousWidth = width_i,
+                RecoveredWidth = PopulationBarWidth - width_s - width_i,
+            };
+        }
+    }
+
+    public struct PopulationLayout
+    {
+        public int SusceptibleWidth { get; set; }
+        public int InfectiousWidth { get; set; }
+        public int RecoveredWidth { get; set; }
     }
 }
